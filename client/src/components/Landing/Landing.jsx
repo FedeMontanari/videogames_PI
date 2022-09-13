35699c05d26c36e11f102d44ded9d21447510bf3
Federico Montanari <fedemontanari@outlook.com>
2022-09-13 12:01:00 -0300

    Modificado video de landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Landing.css"
-import background from  "../../assets/background.mp4" 
+import background from  "../../assets/background.webm" 
 
 function Landing () {
     return (
         <div className="hero">
             <video className='backVideo' autoPlay loop muted>
-                <source src={background} type="video/mp4" />
+                <source src={background} type="video/webm" />
             </video>
             <div className="content">
                 <h1 className="landingTitle">App name</h1>
